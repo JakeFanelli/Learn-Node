e8a5da00fcb94010e541fa3c4ae789088f1b9296
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-21 23:23:51 -0400

    day1

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const jake = { name: "Jake", age: 23, cool: true };
+  //res.send("Hey! It works!");
+  //res.send(jake);
+  //res.send(res.query.name);
+  res.json(req.query);
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
