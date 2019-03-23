1b1bdb610d3a0a69733484519be7e6f204af7374
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-22 23:30:10 -0400

    ok ready for fun

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,22 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController");
 
 // Do work here
-router.get("/", (req, res) => {
-  //const jake = { name: "Jake", age: 23, cool: true };
-  //res.send("Hey! It works!");
-  //res.send(jake);
-  //res.send(res.query.name);
-  //res.json(req.query);
-  res.render("hello", {
-    name: "jake",
-    dog: req.query.dog
-  });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse);
-});
+router.get("/", storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
