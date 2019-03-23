c4fb6b0803f79635ddd17c9e3574f0638a1df19a
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-22 20:04:22 -0400

    lerarning templating

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get("/", (req, res) => {
-  const jake = { name: "Jake", age: 23, cool: true };
+  //const jake = { name: "Jake", age: 23, cool: true };
   //res.send("Hey! It works!");
   //res.send(jake);
   //res.send(res.query.name);
-  res.json(req.query);
+  //res.json(req.query);
+  res.render("hello", {
+    name: "jake",
+    dog: req.query.dog
+  });
 });
 
 router.get("/reverse/:name", (req, res) => {
