1263057d1e8add7c8bc27d537e644757d80d6691
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-24 16:51:40 -0400

    adding geo location to form

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -23,3 +23,28 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render("stores", { title: "Stores", stores });
 };
+
+exports.editStore = async (req, res) => {
+  //find store given id
+  const store = await Store.findOne({ _id: req.params.id });
+  //confirm they are owner of the store
+  //TODO
+  //render out the edit form so user can update
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  //find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, //return the new store instead of old one
+    runValidators: true
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store -></a>`
+  );
+  //redirect them to the store and say it worked
+  res.redirect(`/stores/${store._id}/edit`);
+};
