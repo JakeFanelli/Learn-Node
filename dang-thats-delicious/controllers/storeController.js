4170f449ff01b0269bebf9bf0620bd623ee6b6c5
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-24 01:17:06 -0400

    returning data from db and sending it to view

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render("index");
 };
 
@@ -11,7 +10,16 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect("/");
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  //query the db for list of stores
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
 };
