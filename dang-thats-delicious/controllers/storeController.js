bed0d134fd56fc5dbd6c5f64cad97243a5eab865
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-30 22:33:27 -0400

    lots of user account related activities were accomplished on this fine day.

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -43,6 +43,7 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await new Store(req.body).save();
   req.flash(
     "success",
@@ -57,11 +58,17 @@ exports.getStores = async (req, res) => {
   res.render("stores", { title: "Stores", stores });
 };
 
+const confirmOwner = (store, user) => {
+  if (!store.author.equals(user._id)) {
+    throw Error("You must own a store in order to edit it!");
+  }
+};
+
 exports.editStore = async (req, res) => {
   //find store given id
   const store = await Store.findOne({ _id: req.params.id });
   //confirm they are owner of the store
-  //TODO
+  confirmOwner(store, req.user);
   //render out the edit form so user can update
   res.render("editStore", { title: `Edit ${store.name}`, store });
 };
@@ -85,7 +92,9 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug });
+  const store = await Store.findOne({ slug: req.params.slug }).populate(
+    "author"
+  );
   if (!store) return next();
   res.render("store", { store, title: store.name });
 };
