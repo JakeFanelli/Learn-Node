ec329ce08b2389d722d3b04d544fe3fc29dd9a9d
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-03-31 22:27:39 -0400

    <3

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -56,7 +56,10 @@ router.post(
   catchErrors(authController.update)
 );
 
+router.get("/map", storeController.mapPage);
+
 //API
 router.get("/api/search", catchErrors(storeController.searchStores));
 router.get("/api/stores/near", catchErrors(storeController.mapStores));
+router.post("/api/stores/:id/heart", catchErrors(storeController.heartStore));
 module.exports = router;
